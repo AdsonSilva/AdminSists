2f0ff3a728c0c7c7351a25e030e3c1db41dbe66a
AdsonSilva <adson.silva@ccc.ufcg.edu.br>
2016-08-29 21:32:32 -0300

    Atividade 9 - Falta a hora atual

--- Atividade9/script.sh
+++ Atividade9/script.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-curl --head -w "RTT %{time_total}\\n" google.com > tst
+
+curl -s --head -w "RTT %{time_total}\\n" $1 > tst.txt
+
+hora=$(cat tst.txt | grep Date | cut -d " " -f 6)
+
+rtt=$(cat tst.txt | grep RTT | cut -d " " -f 2)
+
+echo "RTT: $rtt ms"
+echo "Hora retornada pelo servidor: $hora GMT"
