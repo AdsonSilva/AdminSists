50274c0416025da8e982c60411b2139829cac24b
AdsonSilva <adson.silva@ccc.ufcg.edu.br>
2016-09-20 09:01:57 -0300

    Falta o -z

--- Atividade12/script.sh
+++ Atividade12/script.sh
@@ -7,4 +7,24 @@ arquivo=$(echo $origem | rev | cut -d "/" -f1 | rev)
 
 cd $destino
 
-diretorioAtual=$(ls | grep $arquivo | wc -l)
+jaContem=$(ls | grep $arquivo | wc -l)
+
+if [ $jaContem -eq 0 ]; then
+	echo "Criando backup de $origem em $destino/$arquivo"
+	cp $origem $destino
+	
+
+else
+	plus=0
+
+	while [ $jaContem -ne 0 ]
+	do
+		plus=(($plus + 1))
+		jaContem=$(ls | grep "$arquivo.$plus" | wc -l)
+	done
+	arquivoPlus="$arquivo.$plus"
+	echo "Criando backup de $origem em $destino/$arquivo.$plus"
+	cd $origem
+	cp $arquivo $arquivoPlus
+	mv $arquivoPlus $destino
+fi
