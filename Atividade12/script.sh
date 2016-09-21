9985a559d128e6a04a8e7fb92faf4472a458c78a
AdsonSilva <adson.silva@ccc.ufcg.edu.br>
2016-09-20 22:22:17 -0300

    chega!

--- Atividade12/script.sh
+++ Atividade12/script.sh
@@ -1,30 +1,53 @@
 #!/bin/bash
 
-origem=$1
-destino=$2
+if [ $# -eq 2 ]; then
+	origem=$1
+	destino=$2
+
+elif [ $# -eq 3 ]; then
+	origen=$2
+	destino=$3
+fi
 
 arquivo=$(echo $origem | rev | cut -d "/" -f1 | rev)
 
+diretorioOrigem=$(pwd)
 cd $destino
 
 jaContem=$(ls | grep $arquivo | wc -l)
 
 if [ $jaContem -eq 0 ]; then
-	echo "Criando backup de $origem em $destino/$arquivo"
-	cp $origem $destino
+
+	if [ $# -eq 2 ]; then
+		echo "Criando backup de $origem em $destino/$arquivo"
+		cp $origem $destino
+
+	elif [ $# -eq 3 ]; then
+		echo "Criando backup de $origem em $destino/$arquivo.zip"
+		cd $diretorioOrigem
+		zip "$arquivo.zip" $arquivo
+		mv "$arquivo.zip" $destino
+	fi
+
 	
 
 else
-	plus=0
 
-	while [ $jaContem -ne 0 ]
-	do
-		plus=(($plus + 1))
-		jaContem=$(ls | grep "$arquivo.$plus" | wc -l)
-	done
-	arquivoPlus="$arquivo.$plus"
-	echo "Criando backup de $origem em $destino/$arquivo.$plus"
-	cd $origem
-	cp $arquivo $arquivoPlus
-	mv $arquivoPlus $destino
+	if [ $# -eq 3 ]; then
+		echo "Erro: Arquivo existente $destino/$arquivo.zip"
+	else
+
+		plus=
+
+		while [ $jaContem -ne 0 ]
+		do
+			plus=$(($plus + 1))
+			jaContem=$(ls | grep "$arquivo.$plus" | wc -l)
+		done
+		arquivoPlus="$arquivo.$plus"
+		echo "Criando backup de $origem em $destino/$arquivo.$plus"
+		cd $diretorioOrigem
+		cp $arquivo $arquivoPlus
+		mv $arquivoPlus $destino
+	fi
 fi
